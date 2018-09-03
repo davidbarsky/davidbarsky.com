49a04aa72f46c9d89628eece690c65184178f82d
David Barsky <me@davidbarsky.com>
2018-09-03 18:51:55 -0400

    Added title and responsive viewport thingy.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,9 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <title>David Barsky</title>
           <link rel='stylesheet' href='/_next/static/style.css' />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <body>
           <Main />

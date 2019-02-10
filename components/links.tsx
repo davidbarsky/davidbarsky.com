e4aa6d6b30d28ba85a4c9aacd910486ef6477cf3
David Barsky <dbarsky@amazon.com>
2019-02-10 17:06:58 -0500

    refactor links

--- components/links.tsx
+++ components/links.tsx
@@ -1,9 +1,47 @@
 import * as React from "react";
+import Link from "next/link";
 
 const Links = () => {
+  enum Site {
+    Twitter,
+    Instagram,
+    GitHub,
+    LinkedIn
+  }
+
+  const link = (name: Site) => {
+    switch (name) {
+      case Site.Twitter:
+        return (
+          <Link href="https://twitter.com/thramp">
+            <a>Twitter</a>
+          </Link>
+        );
+      case Site.Instagram:
+        return (
+          <Link href="https://instagram.com/thramp">
+            <a>Instagram</a>
+          </Link>
+        );
+      case Site.GitHub:
+        return (
+          <Link href="https://github.com/davidbarsky">
+            <a>GitHub</a>
+          </Link>
+        );
+      case Site.LinkedIn:
+        return (
+          <Link href="https://linkedin.com/in/davidbarsky">
+            <a>GitHub</a>
+          </Link>
+        );
+    }
+  };
+
   return (
     <p>
-      Elsewhere, you can find me on <a href="https://twitter.com/thramp">Twitter</a>, <a href="https://instagram.com/thramp">Instagram</a>, <a href="https://github.com/davidbarsky">GitHub</a>, and <a href="https://linkedin.com/in/davidbarsky">LinkedIn</a>.
+      You can find me on elsewhere on {link(Site.Twitter)},{" "}
+      {link(Site.Instagram)}, {link(Site.GitHub)}, and {link(Site.LinkedIn)}.
     </p>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,16 @@ function App() {
     <Layout title="David Barsky">
       <h2>Hi, I’m David Barsky.</h2>
       <p>
-        I'm currently at Amazon, where I work on test infrastructure for natural language models. I also work on <a href="https://github.com/awslabs/aws-lambda-rust-runtime">Rust Runtime for AWS Lambda</a> and contribute to the <a href="https://tokio.rs/">Tokio</a> project.
+        I'm currently at Amazon, where I work on test infrastructure for natural
+        language models. I also work on{" "}
+        <a href="https://github.com/awslabs/aws-lambda-rust-runtime">
+          Rust Runtime for AWS Lambda
+        </a>{" "}
+        and contribute to the <a href="https://tokio.rs/">Tokio</a> project.
       </p>
       <p>
-        I graduated from Brandeis University, where I studied computer science and history.
+        I graduated from Brandeis University, where I studied computer science
+        and history.
       </p>
       <Links />
     </Layout>

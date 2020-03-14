b167aef279bbf3b0c5efff0501e70d796588805e
David Barsky <me@davidbarsky.com>
2020-03-14 14:22:36 -0400

    update site

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,17 @@ import "../style.css";
 import * as React from "react";
 import Layout from "../components/layout";
 import Links from "../components/links";
+import Link from "next/link";
 
 function App() {
   return (
     <Layout title="David Barsky">
       <h2>Hi, I’m David Barsky.</h2>
       <p>
-        I'm currently at Amazon, where I work on test infrastructure for natural
-        language models. I also work on{" "}
-        <a href="https://github.com/awslabs/aws-lambda-rust-runtime">
-          Rust Runtime for AWS Lambda
-        </a>{" "}
-        and contribute to the <a href="https://tokio.rs/">Tokio</a> project.
+        I work at Amazon. I've been working on the {" "}
+        <Link href="https://github.com/tokio-rs/tracing">
+          <a>Tracing</a>
+        </Link> library.
       </p>
       <p>
         I graduated from Brandeis University, where I studied computer science

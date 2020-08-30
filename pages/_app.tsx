228c3469154609c8a43faae483c0bcc78b2d0b29
David Barsky <me@davidbarsky.com>
2020-08-30 16:08:49 -0400

    Switch to Zola

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "../style.css";
-
-export const config = {
-  unstable_runtimeJS: false
-};
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
